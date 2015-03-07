6aba283ed61f6a2b01486b435576d4b592daaf04
Mike Parker <meebjunk@optonline.net>
2015-03-07 10:56:36 -0500

    ParseMatches works!

--- parsematches.php
+++ parsematches.php
@@ -17,7 +17,7 @@
 	// Connect to the database.
 	$link = mysql_connect('team102.org:3306', 'team102_webuser', $_SESSION['password']);
 	
-	if (!mysql_select_db('team102_2014', $link)) {
+	if (!mysql_select_db('team102_2015', $link)) {
     		echo sprintf('Could not select database, Err: %s', mysql_error());
     		exit;
 	}
@@ -25,12 +25,13 @@
 	function SaveMatch($match_number, $time)
 	{
 		global $link;
+		global $tournament_id;
 		echo sprintf("Saving match %s, time=%s\n", $match_number, $time);
 		$sql = sprintf("insert into matches (tournament_id, match_number, start_time)
 						 values ('%s', %s, '%s')
 							ON DUPLICATE KEY UPDATE start_time = '%s'
 						 "
-						, "MAR"
+						, mysql_real_escape_string($tournament_id)
 						, mysql_real_escape_string($match_number)
 						, mysql_real_escape_string($time)
 						, mysql_real_escape_string($time)
@@ -38,17 +39,19 @@
 		$insertReturn = mysql_query($sql, $link);
 		if(!$insertReturn)
 			echo sprintf("Error inserting match: %s, Err: %s\n", $match_number, mysql_error());
+//		echo $sql;
 		return null;
 	}
 	function SaveMatchTeam($match_number, $team_number, $alliance, $seq_no)
 	{
 		global $link;
+		global $tournament_id;
 		echo sprintf("Saving match-team %s, %s, %s, %s\n", $match_number, $team_number, $alliance, $seq_no);
 		$sql = sprintf("insert into match_teams (tournament_id, match_number, team_number, alliance, seq_no)
 						 values ('%s', %s, %s, '%s', %s)
 							ON DUPLICATE KEY UPDATE tournament_id = tournament_id
 						 "
-						, "MAR"
+						, mysql_real_escape_string($tournament_id)
 						, mysql_real_escape_string($match_number)
 						, mysql_real_escape_string($team_number)
 						, mysql_real_escape_string($alliance)
@@ -57,11 +60,13 @@
 		$insertReturn = mysql_query($sql, $link);
 		if(!$insertReturn)
 			echo sprintf("Error inserting match_team: %s, %s Err: %s\n", $match_number, $team_number, mysql_error());
+//		echo $sql;
 		return null;
 	}
 	function UpdateMatch($match_number, $red_score, $blue_score)
 	{
 		global $link;
+		global $tournament_id;
 		if(($red_score == null) || ($blue_score == null))
 			return null;
 		echo sprintf("Updating match %s, redScore=%s, blueScore=%s\n", $match_number, $red_score, $blue_score);
@@ -70,27 +75,38 @@
 						"
 						, mysql_real_escape_string($red_score)
 						, mysql_real_escape_string($blue_score)
-						, "MAR"
+						, mysql_real_escape_string($tournament_id)
 						, mysql_real_escape_string($match_number)
 						);
 		$insertReturn = mysql_query($sql, $link);
 		if(!$insertReturn)
 			echo sprintf("Error updating match: %s, Err: %s\n", $match_number, mysql_error());
+//		echo $sql;
 		return null;
 	}
 	/* Use internal libxml errors -- turn on in production, off for debugging */
-	libxml_use_internal_errors(true);
+	libxml_use_internal_errors(false);
 	/* Createa a new DomDocument object */
 	$dom = new DomDocument;
+	
+	// Load the active tournament
 	/* Load the HTML */
 	// NOTE: need to put the correct URL for the competition here.
 	// PERHAPS?: $dom->loadHTMLFile("http://www2.usfirst.org/2014comp/Events/NJCLI/matchresults.html");
-//	$dom->loadHTMLFile("http://www2.usfirst.org/2014comp/Events/NJFLA/matchresults.html");
-	$dom->loadHTMLFile("http://www2.usfirst.org/2014comp/Events/MRCMP/matchresults.html");
+//	$dom->loadHTMLFile("http://www2.usfirst.org/2014comp/Events/NJFLA/matchresults.html");		// 2014
+
+	// NOTE: Set OUR tournament ID here.
+	$tournament_id = "UM";
+	
+	// NOTE: Change FIRST's event id in this URL to select a different tournament.
+	$url = "http://frc-events.usfirst.org/2015/MANDA/qualifications";					// 2015
+	$dom->loadHTMLFile($url);				
+	
 	/* Create a new XPath object */
 	$xpath = new DomXPath($dom);
 	/* Query all <td> nodes containing specified class name */
-	$nodes = $xpath->query("//tr[@style='background-color:#FFFFFF;']//td");
+	$nodes = $xpath->query("//table[@class='clean']//tr//td");					// 2015
+//	$nodes = $xpath->query("//tr[@style='background-color:#FFFFFF;']//td");		// 2014
 //	$nodes = $xpath->query("//td");
 	/* Set HTTP response header to plain text for debugging output */
 	header("Content-type: text/plain");
@@ -99,25 +115,25 @@
 		echo "Node($i): ", $node->nodeValue, "\n";
 	}
 */	
-	// The first node is the time
-	// Second node is the match number
+	// The first node is the match number
+	// Second node is the day and time.
 	// 3rd-5th are the Red Alliance Team Numbers
 	// 6th-8th are the Blue Alliance Team Numbers
 	// 9th-10th are the red score and blue score
 	
-
 	$matchNumber = null;
+	$numMatches = count($nodes);
+	if($numMatches <= 1)			// For some reason returns one node when there are no matches.
+		echo sprintf("No matches found for %s at %s\n", $tournament_id, mysql_real_escape_string($url));
+		
 	foreach ($nodes as $i => $node) {
 		if(($i % 10) == 0)
 		{
-			$time = trim($node->nodeValue);
+			$matchNumber = trim($node->nodeValue);
 		}
 		else if(($i % 10) == 1)
 		{
-			$matchNumber = trim($node->nodeValue);
-			// stop when we get to the quarterfinals.
-			if(strpos($matchNumber, 'Qtr') === 0)
-				break;
+			$time = trim($node->nodeValue);
 			SaveMatch($matchNumber, $time);
 		}
 		else if(($i % 10) == 2)
